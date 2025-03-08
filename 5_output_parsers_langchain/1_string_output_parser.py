9c06c2da14b794cf6d90e5e8599117755315537d
sumitme752 <sumitme752@gmail.com>
2025-03-08 23:14:39 +0530

    string_output_parser

---
+++ 5_output_parsers_langchain/1_string_output_parser.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+# 1st prompt --> detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+# 2nd prompt --> summary
+
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the followling text. /n {text}',
+    input_variables=['text']
+)
+
+prompt1 = template1.invoke({'topic':'black hole'})
+
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text':result.content})
+
+result1 = model.invoke(prompt2)
+
+print(result1.content)
